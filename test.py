effaf291ae26dad78b95bbea09b2b9dec10d3b87
Thayalan <thayalan.20200982@iit.ac.lk>
2023-09-17 01:50:44 +0530

    #1 fix file path names

--- test.py
+++ test.py
@@ -5,14 +5,14 @@ import os
 import sys
 
 def test_load_video():
-    cap = cv2.VideoCapture(os.path.join(sys.path[0],'src\\test_clips\\JP_500x500.mp4'))
+    cap = cv2.VideoCapture(os.path.join(sys.path[0],'src/test_clips/JP_500x500.mp4'))
     assert cap.isOpened()
 
 def test_face_detection():
-    cap = cv2.VideoCapture(os.path.join(sys.path[0],'src\\test_clips\\JP_500x500.mp4'))
+    cap = cv2.VideoCapture(os.path.join(sys.path[0],'src/test_clips/JP_500x500.mp4'))
     success, img = cap.read()
     img , faces = detector.findFaceMesh(img=img)
     assert len(faces) > 0
 
 def test_main():
-    assert main(os.path.join(sys.path[0],'src\\test_clips\\JP_500x500.mp4')) is not None
+    assert main(os.path.join(sys.path[0],'src/test_clips/JP_500x500.mp4')) is not None
